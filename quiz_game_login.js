27834a90a816a63e706fe3c3777df85e036cb24b
VinayakGumber <84370842+VinayakGumber@users.noreply.github.com>
2022-01-06 14:19:25 +0530

    Add files via upload

---
+++ quiz_game_login.js
@@ -0,0 +1,11 @@
+function add_user()
+{
+    player1_name = document.getElementById("player_1_input").value;
+    player2_name = document.getElementById("player_2_input").value;
+
+    localStorage.setItem("player1_name",player1_name);
+    localStorage.setItem("player2_name",player2_name);
+
+    window.location = "game_page.html";
+    
+}
